b2c37898468f023def0ba72e56f884c6c093b77b
minjichu <key0876@naver.com>
2018-10-02 15:45:44 +0900

    03_ sec

--- main.c
+++ main.c
@@ -4,18 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-int op1, op2;
-int sum, sub;
-printf("input integers :");
-scanf("%i %i", &op1, &op2);
+int sec;
+printf("input :");
+scanf("%d", &sec);
+printf("%d sec, is %i : %i\n", sec, sec/60, sec%60);
 
-sum = op1 + op2;
-sub = op1 - op2;
-
-printf("+ operation  : %i\n", sum);
-printf("- operation : %i\n", sub);
-printf("* operation : %i\n", op1*op2);
-printf("/ operation : %i\n", op1/op2);
-printf("%% operation: %i\n", op1%op2);
 	return 0;
 }
